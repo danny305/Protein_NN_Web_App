e00e8ed38108bf7e8ec47eb07d451ed5009a6905
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2019-02-12 20:05:09 -0600

    Editted CSS, removed freesasa from requirements.txt and edited entrypoint.sh to include nohup and run as a bg process.

--- web_app/entrypoint.sh
+++ web_app/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source ./flask_env.sh -P
+#source ./flask_env.sh -P
+source ./flask_env.sh -D
 
-#gunicorn --bind 0.0.0.0:8000 --log-level=debug wsgi:app
+# nohup gunicorn --bind 0.0.0.0:8000 --log-level=debug wsgi:app &
+# nohup gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app &
 gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app
 
 #This command will only work on a linux server and is not for running within a docker container.np

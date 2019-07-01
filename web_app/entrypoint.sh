af31144368eeb1df3d02dceb39af12b25ebaaf45
Ubuntu <ubuntu@ip-172-31-0-66.us-east-2.compute.internal>
2019-07-01 13:33:34 -0500

    All changes made to the website before adding the changes I made on my local computer. The changes on my local computer should just be content.

--- web_app/entrypoint.sh
+++ web_app/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 #source ./flask_env.sh -P
-source ./flask_env.sh -D
+source ./flask_env.sh -P
 
+#make sure you check which ip and port nginx is forwarding all traffic to.
+	# This is when you are running gunicorn without nginx
 # nohup gunicorn --bind 0.0.0.0:8000 --log-level=debug wsgi:app &
-# nohup gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app &
-gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app
+	# This is when nginx is is forwarding traffic to localhost.
+nohup gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app &
+	#This is when you are testing gunicorn in development on you localhost
+# gunicorn --bind 127.0.0.1:8000 --log-level=debug wsgi:app
 
 #This command will only work on a linux server and is not for running within a docker container.np
-# sudo systemctl start nginx
+sudo systemctl start nginx
+echo 'Starting Nginx as a proxy server.'
